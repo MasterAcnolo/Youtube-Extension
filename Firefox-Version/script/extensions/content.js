d2cc8dcbaa5f26b8964c6eb68718b5b9be9be14a
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2025-05-04 21:13:48 +0200

    Firefox OK

---
+++ Firefox-Version/script/extensions/content.js
@@ -0,0 +1,57 @@
+/**
+ * MonExtension - Extension pour rediriger les vidéos YouTube vers youtube-nocookie.
+ * Copyright (C) 2025 MasterAcnolo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+// Compatibilité Firefox / Chrome
+if (typeof browser === "undefined") {
+  var browser = chrome;
+}
+
+
+function redirectToNoCookie() {
+    const url = new URL(window.location.href);
+    const videoId = url.searchParams.get("v");
+  
+    if (videoId) {
+      const newUrl = `https://www.youtube-nocookie.com/embed/${videoId}`;
+      window.location.replace(newUrl);
+    }
+  }
+  
+  chrome.storage.sync.get(["enabled"], (result) => {
+    const isEnabled = result.enabled ?? true;
+    if (isEnabled && window.location.href.includes("watch?v=")) {
+      redirectToNoCookie();
+    }
+  });
+  
+  const observer = new MutationObserver(() => {
+    if (window.location.href.includes("watch?v=")) {
+      chrome.storage.sync.get(["enabled"], (result) => {
+        const isEnabled = result.enabled ?? true;
+        if (isEnabled) {
+          redirectToNoCookie();
+        }
+      });
+    }
+  });
+  
+  observer.observe(document, {
+    subtree: true,
+    childList: true
+  });
+  
